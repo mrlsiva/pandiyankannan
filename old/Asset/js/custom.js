27910e56e3906702f09b1da9fabe5c60f9f57a87
mrlsiva <54312287+mrlsiva@users.noreply.github.com>
2023-11-09 16:05:59 +0530

    first code

---
+++ old/Asset/js/custom.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+    $('a[href*=#]').bind('click', function(e) {
+        e.preventDefault(); // prevent hard jump, the default behavior
+
+        var target = $(this).attr("href"); // Set the target as variable
+
+        // perform animated scrolling by getting top-position of target-element and set it as scroll target
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 600, function() {
+            // location.hash = target; 
+
+            //attach the hash (#jumptarget) to the pageurl
+        });
+
+        return false;
+    });
+    // debugger
+    // $('.nav-link').has('.active').parent().css({ "border": "1px solid #000" });
+});
+
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+    // Show/hide menu on scroll
+    //if (scrollDistance >= 850) {
+    //		$('nav').fadeIn("fast");
+    //} else {
+    //		$('nav').fadeOut("fast");
+    //}
+
+    // Assign active class to nav links while scolling
+    $('.page-section').each(function(i) {
+        if ($(this).position().top <= scrollDistance) {
+            $('.navigation a.active').removeClass('active');
+            $('.navigation a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
+
+// Trigger CSS animations on scroll.
+// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
+
+// Looking for a version that also reverses the animation when
+// elements scroll below the fold again?
+// --> Check https://codepen.io/bramus/pen/vKpjNP
+
+jQuery(function($) {
+
+    // Function which adds the 'animated' class to any '.animatable' in view
+    var doAnimations = function() {
+
+        // Calc current offset and get all animatables
+        var offset = $(window).scrollTop() + $(window).height(),
+            $animatables = $('.animatable');
+
+        // Unbind scroll handler if we have no animatables
+        if ($animatables.length == 0) {
+            $(window).off('scroll', doAnimations);
+        }
+
+        // Check all animatables and animate them if necessary
+        $animatables.each(function(i) {
+            var $animatable = $(this);
+            if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+                $animatable.removeClass('animatable').addClass('animated');
+            }
+        });
+
+    };
+
+    // Hook doAnimations on scroll, and trigger a scroll
+    $(window).on('scroll', doAnimations);
+    $(window).trigger('scroll');
+
+});

b9007c1abc1800f9c95bb5e5fa178925e48ac8fa
mrlsiva <54312287+mrlsiva@users.noreply.github.com>
2023-11-14 20:13:24 +0530

    sdv

--- works/index.php
+++ works/index.php
@@ -4,73 +4,15 @@
 <head>
     <meta charset="utf-8">
 <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
-<meta content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1" name="viewport">
-<meta content="Madurai Maharaja Biriyani" name="apple-mobile-web-app-title">
-<meta content="Madurai Maharaja Biriyani" name="application-name">
-<meta content="#ff0000 " name="theme-color">
+<title>Pandiyan Kannan</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta content="MM Biriyani (also known as Madurai Maharaja Biriyani) is a restaurant chain that operates primarily in the Indian state of Tamil Nadu. The first outlet was opened in 2019 at Madurai. Since then, it operates over 2 outlets in TamilNadu. The MM Biriyani restaurants focus on biriyani. The founder, Alla Basha & Vel, started MM Biriyani restaurant by the name of Madurai Maharaja Biriyani in Madurai, Tamil Nadu." data-react-helmet="true" name="description">
-<meta content="Madurai Maharaja Biriyani, Food delivery services" data-react-helmet="true" property="og:title">
-<meta content="Madurai Maharaja Biriyani" data-react-helmet="true" property="og:site_name">
-<meta content="maduraimaharajabiriyani.com" data-react-helmet="true" property="og:url">
-<meta content="MM Biriyani (also known as Madurai Maharaja Biriyani) is a restaurant chain that operates primarily in the Indian state of Tamil Nadu. The first outlet was opened in 2019 at Madurai. Since then, it operates over 2 outlets in TamilNadu. The MM Biriyani restaurants focus on biriyani. The founder, Alla Basha & Vel, started MM Biriyani restaurant by the name of Madurai Maharaja Biriyani in Madurai, Tamil Nadu." data-react-helmet="true" property="og:description">
-<meta content="https://www.facebook.com/mmbiryani1" data-react-helmet="true" property="article:author">
-<meta content="https://www.maduraimaharajabiriyani.com/assets/img/Share.png?v1" data-react-helmet="true" property="og:image">
-<meta content="image/jpeg" data-react-helmet="true" property="og:image:type">
-<meta content="300" data-react-helmet="true" property="og:image:width">
-<meta content="300" data-react-helmet="true" property="og:image:height">
-<meta property="og:image" content="https://www.maduraimaharajabiriyani.com/assets/img/Share.png?v1">
-<title>Madurai Maharaja Biriyani - Food delivery services</title>
-<link rel='icon' href="https://www.maduraimaharajabiriyani.com/assets/img/favicon.png" type='image/x-icon' >
 
-    <link href="https://www.maduraimaharajabiriyani.com/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Redressed&display=swap" rel="stylesheet">
-    <!-- Vendor CSS Files -->
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
-    <link href="assets/css/style.css?v3" rel="stylesheet">
 </head>
 
 <body>
 
 
     <main id="main">
-
-        <!-- ======= About Section ======= -->
-        <section id="about" class="about">
-            <div class="container" data-aos="fade-up">
-
-                <div class="row">
-                    <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="100">
-                        <div class="about-img">
-                            <img src="assets/img/about.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
-                        <h3>Madurai Maharaja Biriyani</h3>
-                        <p class="font-italic">
-                            MM Biriyani (also known as Madurai Maharaja Biriyani) is a restaurant chain that operates primarily in the Indian state of Tamil Nadu. The first outlet was opened in 2019 at Madurai. Since then, it operates over 2 outlets in TamilNadu. The MM Biriyani
-                            restaurants focus on biriyani. The founder, Alla Basha & Vel, started MM Biriyani restaurant by the name of Madurai Maharaja Biriyani in Madurai, Tamil Nadu.
-                        </p>
-                       
-                    </div>
-                </div>
-
-            </div>
-        </section>
-        
-
-
-
-        <!-- End About Section -->
 
         <!-- ======= Menu Section ======= -->
         <section id="menu" class="menu section-bg">

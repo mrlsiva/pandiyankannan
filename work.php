12cda5e8d32123cbb692d9c823937ea2e6bcc075
mrlsiva <54312287+mrlsiva@users.noreply.github.com>
2023-12-02 18:25:08 +0530

    works

--- work.php
+++ work.php
@@ -64,7 +64,7 @@
             <div class="row">
                 <div class="row motion-effects motion-effects-layer  col-md-12 project-tit">
                     <div class="font-cover">
-                        <h1 class="font-sizee1 main-text slideInUp text-center">WEIRDOS</h1>
+                        <h1 class="font-sizee1 main-text slideInUp text-center">'.$row["ProductName"].'</h1>
                     </div>
                 </div>
             </div>

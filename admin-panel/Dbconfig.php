5e8e8e1b6b0bacef802147000fe25567ee1809c4
Arun <arunsubramaniyan208@gmail.com>
2023-12-15 01:03:32 -0800

    pdf changed

---
+++ admin-panel/Dbconfig.php
@@ -0,0 +1,11 @@
+
+<?php
+
+//Dbconfig.php
+
+// $connect = new PDO('mysql:host=localhost;dbname=data_img', 'root', '');
+// $connect = new PDO('mysql:host=localhost;dbname=slinggro_pandiyan', 'slinggro_pandiyan', '$wKa-1YmwXs%');
+$connect = new PDO('mysql:host=localhost;dbname=pandiyan', 'root', '');
+// $con = mysqli_connect("localhost","slinggro_siva","sivA@12$","slinggro_mmbiriyani");
+
+?>

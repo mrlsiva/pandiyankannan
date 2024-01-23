6f5842d92156a65ab1cfa6f2a7a81e499a553c65
Arun <arunsubramaniyan208@gmail.com>
2024-01-23 16:51:48 +0530

    db

--- admin-panel/Dbconfig.php
+++ admin-panel/Dbconfig.php
@@ -4,8 +4,8 @@
 //Dbconfig.php
 
 // $connect = new PDO('mysql:host=localhost;dbname=data_img', 'root', '');
-// $connect = new PDO('mysql:host=localhost;dbname=slinggro_pandiyan', 'slinggro_pandiyan', '$wKa-1YmwXs%');
-$connect = new PDO('mysql:host=localhost;dbname=pandiyan', 'root', '');
+$connect = new PDO('mysql:host=localhost;dbname=slinggro_pandiyan', 'slinggro_pandiyan', '$wKa-1YmwXs%');
+// $connect = new PDO('mysql:host=localhost;dbname=pandiyan', 'root', '');
 // $con = mysqli_connect("localhost","slinggro_siva","sivA@12$","slinggro_mmbiriyani");
 
 ?>

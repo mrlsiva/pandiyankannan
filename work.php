f2ad483b3e7a76df1fa2220e3c33422a852ca08a
Arun <arunsubramaniyan208@gmail.com>
2024-01-24 15:47:46 +0530

    FAV ADDED

--- work.php
+++ work.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pandiyan Kannan Works</title>
-
+	<link rel="icon" type="image/x-icon" href="assests/images/fav.png">
     <!-- Bootstrap CSS -->
 
     <link rel="stylesheet" href="assests/css/style.css">

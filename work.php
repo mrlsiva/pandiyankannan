d830c720bdbee96fe8ad7adaddccdc741be65c66
mrlsiva <54312287+mrlsiva@users.noreply.github.com>
2024-02-01 15:45:34 +0530

    css changes

--- work.php
+++ work.php
@@ -25,6 +25,15 @@
         .row-spinner {
             font-size: 40px;
         }
+        .star-img {
+            width:70px;
+            margin: auto;
+
+        }
+        @media screen and (max-width:768px){ 
+            width:28px;
+
+        }
     </style>
 </head>
 
@@ -68,7 +77,7 @@
                 </div>
                 <div class="col-4 col-md-4">
                     <H5 class="text-center row-spinner slideInUp">
-                        <img class="star-img spinner" style="margin: auto; width: 70px;"
+                        <img class="star-img spinner star-img"
                             src="assests/images/asterisk-1.svg" alt="Extraordinary Spinner">
                         <span></span>
                     </H5>

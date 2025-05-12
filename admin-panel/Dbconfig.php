158d75539223d8aa208e8123bb64623bfdf621bd
siva <pmrlsivas@gmail.com>
2025-05-12 17:52:51 +0530

    db

--- admin-panel/Dbconfig.php
+++ admin-panel/Dbconfig.php
@@ -3,8 +3,8 @@
 
 //Dbconfig.php
 
-$connect = new PDO("mysql:host=127.0.0.1;port=3307;dbname=pandiyan", "root", "");
-// $connect = new PDO('mysql:host=localhost;dbname=slinggro_pandiyan', 'slinggro_pandiyan', '$wKa-1YmwXs%');
+// $connect = new PDO("mysql:host=127.0.0.1;port=3307;dbname=pandiyan", "root", "");
+$connect = new PDO('mysql:host=localhost;dbname=slinggro_pandiyan', 'slinggro_pandiyan', '$wKa-1YmwXs%');
 // $connect = new PDO('mysql:host=localhost;dbname=pandiyan', 'root', '');
 // $con = mysqli_connect("localhost","slinggro_siva","sivA@12$","slinggro_mmbiriyani");
 

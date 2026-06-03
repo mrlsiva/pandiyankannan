4d6545a15ee27b040163c0d42b2ca59747492291
Francis <francis.francis20.1989@gmail.com>
2026-06-03 11:33:06 +0530

    db

--- admin-panel/Dbconfig.php
+++ admin-panel/Dbconfig.php
@@ -4,7 +4,7 @@
 //Dbconfig.php
 
 // $connect = new PDO('mysql:host=localhost;dbname=data_img', 'root', '');
-$connect = new PDO('mysql:host=localhost;dbname=slinggro_pandiyan', 'slinggro_pandiyan', '$wKa-1YmwXs%');
+$connect = new PDO('mysql:host=localhost;dbname=sql_pandiyankannan_com', 'sql_pandiyankannan_com', 'f9c1afb7b7e6d');
 // $connect = new PDO('mysql:host=localhost;dbname=pandiyan', 'root', '');
 // $connect = new PDO('mysql:host=localhost;dbname=mmb', 'root', '');
 // $con = mysqli_connect("localhost","slinggro_siva","sivA@12$","slinggro_mmbiriyani");
